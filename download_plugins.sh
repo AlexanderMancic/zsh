72a8fe514ab78c4ea9e6161815dd71e45a2e699d
AlexanderMancic <mancicalex@gmail.com>
2025-09-19 21:42:47 +0200

    add plugin script to automatically git clone plugin repo (closes #3)

---
+++ download_plugins.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PLUGINS_DIR="$HOME/.config/zsh/plugins/"
+
+mkdir -p "$PLUGINS_DIR"
+cd "$PLUGINS_DIR"
+
+git clone https://github.com/romkatv/powerlevel10k.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git
+git clone https://github.com/Aloxaf/fzf-tab.git
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+
